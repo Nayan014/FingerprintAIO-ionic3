ad299681d8f7676963e6d4f80bd75bb68062f470
Nayan <nayan.v@crestinfosystems.net>
2017-12-26 12:42:06 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Source: <br />
+https://ionicframework.com/docs/native/fingerprint-aio/
+
+Works for ANDROID
+
+$ ionic cordova plugin add cordova-plugin-fingerprint-aio
+$ npm install --save @ionic-native/fingerprint-aio
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private faio: FingerprintAIO) {
+  }
 
+  scan(){
+    this.faio.show({
+      clientId: 'Fingerprint-Demo',
+      clientSecret: 'password', //Only necessary for Android
+      disableBackup:true,  //Only for Android(optional)
+      localizedFallbackTitle: 'Use Pin', //Only for iOS
+      localizedReason: 'Please authenticate' //Only for iOS
+  })
+  .then((result: any) => 
+    this.navCtrl.push('ResultPage',{
+      result: result
+    })
+  )
+  .catch((error: any) =>  
+    this.navCtrl.push('ResultPage',{
+      result: error
+    }));
   }
 
 }
